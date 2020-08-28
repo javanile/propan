6d26675faa7c00a0aaf9c8d9397fd30a8b14c77e
Francesco Bianco <info.francescobianco@gmail.com>
2020-08-28 13:52:44 +0200

    proof of concept #8

---
+++ src/Activities/BuildTemplates/HerokuNginxConf.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Javanile\Propan\Activities\BuildTemplates;
+
+class HerokuNginxConf
+{
+    public static function fileContent()
+    {
+        return <<<EOF
+location / {
+    try_files \$uri @rewriteapp;
+}
+
+location @rewrite {
+    rewrite ^(.*)$ /index.php/$1 last;
+}
+
+location ~ ^/index\.php(/|$) {
+    try_files @heroku-fcgi @heroku-fcgi;
+    internal;
+}
+EOF;
+    }
+}
